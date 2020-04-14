15b208dafc734ef6bf0b8684f1b276d70575ea66
kazhe94 <evgeniisherbakov@mail.ru>
2020-04-14 20:46:58 +0300

    мелкие правки

--- news/script.js
+++
@@ -1,9 +0,0 @@
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault;
-    $(this).toggleClass('menu-btn_active');
-    $('.mobile-nav').slideToggle('slow');
-    $('.mobile-props-block').toggle('fast');
-    $('body').toggleClass('stop-scroll');
-    $('body').bind('touchmove', function(e){e.preventDefault()});
-
-});  
